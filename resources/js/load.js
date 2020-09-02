4e1ef5c41fe2fe9a10d5706566610b1eadaf458b
Gustavo Gómez <gustavo@mvagency.co>
2020-09-02 08:19:01 -0600

    first commit
    First commit

---
+++ resources/js/load.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var contenedor = document.getElementById('load-container');
+
+    setTimeout(function(){
+        contenedor.style.visibility = "hidden";
+        contenedor.style.opacity = '0';
+    },500)
+};
---
+++ resources/js/main.js
@@ -0,0 +1,53 @@
+var showMenuBox = true;
+var showFriendsBox = false;
+
+$(document).ready(function(){
+    showHome()
+
+    $('.menu-box').css('left','-350px');
+    $('.friends-box').css('right','-350px');
+
+    $('.menu-button').click(function(){
+        toggleMenu()
+    });
+
+
+});
+
+let showHome=()=>{
+    $('#history').slideUp(0);
+    $('#home').slideDown(0);
+    toggleMenu()
+}
+
+let showHistory=()=>{
+    $('#home').slideUp(0);
+    $('#history').slideDown(0);
+    toggleMenu()
+}
+
+let toggleMenu=()=>{
+    if(showMenuBox){
+        $('.menu-box').css('left','-350px');
+        //$('.menu-box').hide(500);
+        showMenuBox = false;
+    }else{
+        if(showFriendsBox){toggleFriendBox()}
+        $('.menu-box').show(300);
+        $('.menu-box').css('left','0px');
+        showMenuBox = true;
+    }
+}
+
+let toggleFriendBox=()=>{
+    if(showFriendsBox){
+        $('.friends-box').css('right','-350px');
+        //$('.menu-box').hide(500);
+        showFriendsBox = false;
+    }else{
+        if(showMenuBox){toggleMenu()}
+        $('.friends-box').show(300);
+        $('.friends-box').css('right','0px');
+        showFriendsBox = true;
+    }
+}
